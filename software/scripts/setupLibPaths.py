9c7f3e949e3f04cb49f8649709679e233e6879df
Bruce Hill <bhill@slac.stanford.edu>
2019-12-19 23:01:42 -0800

    Make setupLibPaths.py more robust so can be imported from unrelated directories.
    Needed for embedded python import of setupLibPaths.py from C++ from other apps.

--- software/scripts/setupLibPaths.py
+++ software/scripts/setupLibPaths.py
@@ -11,7 +11,7 @@
 import pyrogue as pr
 import os
 
-top_level = os.getcwd().split('software')[0]
+top_level = os.path.realpath(__file__).split('software')[0]
 
 pr.addLibraryPath(top_level+'firmware/submodules/axi-pcie-core/python')
 pr.addLibraryPath(top_level+'firmware/submodules/lcls2-pgp-fw-lib/python')
