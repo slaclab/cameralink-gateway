3e0481deb4fa4fb2a447a5c7caf87c45027f476d
Larry Ruckman <ruckman@slac.stanford.edu>
2020-02-28 10:18:33 -0800

    adding option for PyDm GUI or PyQt GUI

--- software/scripts/devGui.py
+++ software/scripts/devGui.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 #-----------------------------------------------------------------------------
-# This file is part of the 'Camera link gateway'. It is subject to 
-# the license terms in the LICENSE.txt file found in the top-level directory 
-# of this distribution and at: 
-#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
-# No part of the 'Camera link gateway', including this file, may be 
-# copied, modified, propagated, or distributed except according to the terms 
+# This file is part of the 'Camera link gateway'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'Camera link gateway', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
 
@@ -17,82 +17,90 @@ import rogue
 import pyrogue.gui
 import pyrogue.pydm
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
 
-    #################################################################
+#################################################################
 
-        # Set the argument parser
-        parser = argparse.ArgumentParser()
+    # Set the argument parser
+    parser = argparse.ArgumentParser()
 
-        # Convert str to bool
-        argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
+    # Convert str to bool
+    argBool = lambda s: s.lower() in ['true', 't', 'yes', '1']
 
-        # Add arguments
-        parser.add_argument(
-            "--dev", 
-            type     = str,
-            required = False,
-            default  = '/dev/datadev_0',
-            help     = "path to device",
-        )  
+    # Add arguments
+    parser.add_argument(
+        "--dev",
+        type     = str,
+        required = False,
+        default  = '/dev/datadev_0',
+        help     = "path to device",
+    )
 
-        parser.add_argument(
-            "--hwType", 
-            type     = str,
-            required = False,
-            default  = 'kcu1500',
-            help     = "kcu1500 or SlacPgpCardG4",
-        )  
+    parser.add_argument(
+        "--hwType",
+        type     = str,
+        required = False,
+        default  = 'kcu1500',
+        help     = "kcu1500 or SlacPgpCardG4",
+    )
 
-        parser.add_argument(
-            "--camType", 
-            nargs    ='+',
-            required = True,
-            help     = "List of camera type",
-        )  
+    parser.add_argument(
+        "--camType",
+        nargs    ='+',
+        required = True,
+        help     = "List of camera type",
+    )
 
-        parser.add_argument(
-            "--pollEn", 
-            type     = argBool,
-            required = False,
-            default  = True,
-            help     = "Enable auto-polling",
-        ) 
+    parser.add_argument(
+        "--pollEn",
+        type     = argBool,
+        required = False,
+        default  = True,
+        help     = "Enable auto-polling",
+    )
 
-        parser.add_argument(
-            "--initRead", 
-            type     = argBool,
-            required = False,
-            default  = True,
-            help     = "Enable read all variables at start",
-        )  
+    parser.add_argument(
+        "--initRead",
+        type     = argBool,
+        required = False,
+        default  = True,
+        help     = "Enable read all variables at start",
+    )
 
-        parser.add_argument(
-            "--defaultFile", 
-            type     = str,
-            required = False,
-            default  = None,
-            help     = "Sets the default YAML configuration file to be loaded at the root.start()",
-        ) 
+    parser.add_argument(
+        "--defaultFile",
+        type     = str,
+        required = False,
+        default  = None,
+        help     = "Sets the default YAML configuration file to be loaded at the root.start()",
+    )
 
-        parser.add_argument(
-            "--serverPort", 
-            type     = int,
-            required = False,
-            default  = 9099,
-            help     = "Zeromq server port",
-        )
+    parser.add_argument(
+        "--serverPort",
+        type     = int,
+        required = False,
+        default  = 9099,
+        help     = "Zeromq server port",
+    )
 
-        parser.add_argument(
-            "--releaseZip", 
-            type     = str,
-            required = False,
-            default  = None,
-            help     = "Sets the default YAML configuration file to be loaded at the root.start()",
-        ) 
+    parser.add_argument(
+        "--releaseZip",
+        type     = str,
+        required = False,
+        default  = None,
+        help     = "Sets the default YAML configuration file to be loaded at the root.start()",
+    )
 
-        # Get the arguments
-        args = parser.parse_args()
+    parser.add_argument(
+        "--guiType",
+        type     = str,
+        required = False,
+        default  = 'PyDm',
+        help     = "Sets the GUI type (PyDm or PyQt)",
+    )
+
+    # Get the arguments
+    args = parser.parse_args()
 
     #################################################################
 
@@ -136,6 +144,24 @@ if __name__ == "__main__":
             clDevTarget = clDevTarget,
         ) as root:
 
-        pyrogue.pydm.runPyDM(root=root)
-        
+        if (args.guiType == 'PyDm'):
+
+            pyrogue.pydm.runPyDM(root=root)
+
+        elif (args.guiType == 'PyQt'):
+
+            # Create GUI
+            appTop = pyrogue.gui.application(sys.argv)
+            guiTop = pyrogue.gui.GuiTop()
+            guiTop.addTree(root)
+            guiTop.resize(800, 1000)
+
+            # Run gui
+            appTop.exec_()
+            root.stop()
+
+        # Undefined device type
+        else:
+            raise ValueError("Invalid GUI type (%s)" % (args.guiType) )
+
     #################################################################
