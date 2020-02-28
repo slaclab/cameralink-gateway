7dfcac7639f1c8f9ff5626e293e942e79aef83f8
Larry Ruckman <ruckman@slac.stanford.edu>
2020-02-28 14:51:49 -0800

    preparing for release

--- firmware/targets/shared_pcie_config.mk
+++ firmware/targets/shared_pcie_config.mk
@@ -1,5 +1,5 @@
-# Define Firmware Version: v4.0.0.1
-export PRJ_VERSION = 0x04000001
+# Define Firmware Version: v4.0.0.2
+export PRJ_VERSION = 0x04000002
 
 # Prom type: mt25qu512-spi-x1_x2_x4_x8
 target: prom
