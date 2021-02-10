5df53ddd1f52691c713b877ea71a285a0a8bdb8f
Larry Ruckman <ruckman@slac.stanford.edu>
2021-02-09 17:34:04 -0800

    preparing for release

--- firmware/python/cameralink_gateway/_ClinkDevRoot.py
+++ firmware/python/cameralink_gateway/_ClinkDevRoot.py
@@ -44,7 +44,7 @@ class ClinkDevRoot(shared.Root):
                  **kwargs):
 
         # Set the firmware Version lock = firmware/targets/shared_version.mk
-        self.FwVersionLock = 0x06010000
+        self.FwVersionLock = 0x07000000
 
         # Set number of lanes to min. requirement
         if numLanes > len(camType):
