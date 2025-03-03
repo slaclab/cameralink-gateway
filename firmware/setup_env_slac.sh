129196c1e92a767d3d0baf36b4526dd31c7802d2
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-03 10:46:18 -0800

    updating setup scripts for SDF

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -1,2 +1,1 @@
-# Setup the Xilinx software and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2020.1/Vivado/2020.1/settings64.sh
+source /sdf/group/faders/tools/xilinx/2023.1/Vivado/2023.1/settings64.sh
